115a8c2648ed6e161e4595aa2cfeae4b994164fa
Bulent Coskun <bulent.coskun@gmail.com>
2022-02-03 10:41:00 -0500

    inital code

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace bm3soft.data.protector;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            var versionString = Assembly.GetEntryAssembly()?
+                                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                    .InformationalVersion
+                                    .ToString();
+
+            Console.WriteLine($"data-protector v{versionString}");
+            Console.WriteLine("-------------");
+            Console.WriteLine("\nUsage:");
+            Console.WriteLine("  data-protector [namespace] [plaintext] [silence flag 'true' if you want to silence mode]");
+            return;
+        }
+        
+        var nspace_value = args[0];
+        var plaintext = args[1];  
+        var silenceFlag = false;
+
+        if ( args.Length > 2)
+        silenceFlag = Convert.ToBoolean(args[2]);
+
+        MainLogic("Welcome to Data-Protector!", nspace_value, plaintext, silenceFlag);
+    }
+
+static void MainLogic(string message, string nspace, string plaintext, bool silenceFlag  )
+{
+    string logo = $"\n        \t{message}";
+    logo += @"
+        ........................................
+        ........................................
+        ........................................
+        ...............WWNNNNNNWW...............
+        .............WNXNNWWWWNNXNW.............
+        .............NXN........NXN.............
+        ............WXXW........WXXW............
+        ............WXNW........WNNW............
+        ............NXXNNNNWWWNNNNNW............
+        ...........WKOkkOOOOOOOOOOOKW...........
+        ...........N0kxxxxdoodxxxkk0W...........
+        ...........N0kxxxx:..;dxxkk0W...........
+        ...........N0kxxxx:..,dxxkk0N...........
+        ...........N0kxxxxl;,:dxxkk0N...........
+        ...........WX0OOOOOOOOOOOO0XW...........
+        ............WWWWWWWWWWWWWWWW............
+        ........................................
+        ........................................
+        ........................................
+        ........................................
+        ";
+
+    if (!silenceFlag)
+        Console.WriteLine(logo);
+
+ 
+
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddDataProtection();
+    var services = serviceCollection.BuildServiceProvider();
+    
+    var instance = ActivatorUtilities.CreateInstance<ProtectionManager>(services, nspace);
+    instance.Encyrpt(plaintext);
+
+}
+
+ public class ProtectionManager
+    {
+        IDataProtector _protector;
+
+        // the 'provider' parameter is provided by DI
+        public ProtectionManager(IDataProtectionProvider provider, string nspace)
+        {
+            _protector = provider.CreateProtector(nspace);
+        }
+
+
+/*
+        public void RunSample()
+        {
+            Console.Write("Enter input: ");
+            string input = Console.ReadLine();
+
+            // protect the payload
+            string protectedPayload = _protector.Protect(input);
+            Console.WriteLine($"Protect returned: {protectedPayload}");
+
+            // unprotect the payload
+            string unprotectedPayload = _protector.Unprotect(protectedPayload);
+            Console.WriteLine($"Unprotect returned: {unprotectedPayload}");
+        }
+*/
+
+        internal void Encyrpt(string plaintext)
+        {
+            if ( String.IsNullOrEmpty(plaintext) ) throw new ArgumentException("plaintext cannot be null");            
+            string protectedPayload = _protector.Protect(plaintext);
+            Console.WriteLine($"{protectedPayload}");
+        }
+    }
+
+}
